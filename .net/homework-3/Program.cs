9e0251db8ae0322d7316f53438f78a81a2deda6d
Mark Semenov <semenovmark07@gmail.com>
2025-03-04 04:24:03 +0200

    init + homework-1, homework-3, homework-4

---
+++ .net/homework-3/Program.cs
@@ -0,0 +1,114 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Выберите задание (1-6): ");
+        int choice = int.Parse(Console.ReadLine());
+
+        switch (choice)
+        {
+            case 1:
+                AverageOfThree();
+                break;
+            case 2:
+                PowerCalculation();
+                break;
+            case 3:
+                CurrencyConverter();
+                break;
+            case 4:
+                KmToMilesConverter();
+                break;
+            case 5:
+                PercentageOfNumber();
+                break;
+            case 6:
+                TemperatureConverter();
+                break;
+            default:
+                Console.WriteLine("Неверный ввод.");
+                break;
+        }
+    }
+
+    static void AverageOfThree()
+    {
+        Console.Write("Введите три числа через пробел: ");
+        string[] input = Console.ReadLine().Split();
+        double a = double.Parse(input[0]);
+        double b = double.Parse(input[1]);
+        double c = double.Parse(input[2]);
+
+        double average = (a + b + c) / 3;
+        Console.WriteLine($"Среднее арифметическое: {average}");
+    }
+    
+
+    static void PowerCalculation()
+    {
+        Console.Write("Введите число: ");
+        double number = double.Parse(Console.ReadLine());
+
+        Console.Write("Введите степень: ");
+        double power = double.Parse(Console.ReadLine());
+
+        double result = Math.Pow(number, power);
+        Console.WriteLine($"Результат: {result}");
+    }
+    
+
+    static void CurrencyConverter()
+    {
+        Console.Write("Введите сумму в гривнах: ");
+        double uah = double.Parse(Console.ReadLine());
+
+        double usdRate = 43.0; 
+        double eurRate = 46.0;
+
+        Console.WriteLine($"В долларах: {uah / usdRate:F2} USD");
+        Console.WriteLine($"В евро: {uah / eurRate:F2} EUR");
+    }
+    
+
+    static void KmToMilesConverter()
+    {
+        Console.Write("Введите расстояние в километрах: ");
+        double km = double.Parse(Console.ReadLine());
+
+        double landMiles = km * 0.621371;
+        double nauticalMiles = km * 0.539957;
+
+        Console.WriteLine($"Сухопутные мили: {landMiles:F2}");
+        Console.WriteLine($"Морские мили: {nauticalMiles:F2}");
+    }
+    
+
+    static void PercentageOfNumber()
+    {
+        Console.Write("Введите число N: ");
+        double N = double.Parse(Console.ReadLine());
+
+        Console.Write("Введите процент P: ");
+        double P = double.Parse(Console.ReadLine());
+
+        double result = (P / 100) * N;
+        Console.WriteLine($"{P}% от {N} = {result}");
+    }
+    
+
+    static void TemperatureConverter()
+    {
+        Console.Write("Введите температуру в градусах Цельсия: ");
+        double celsius = double.Parse(Console.ReadLine());
+        double fahrenheit = (celsius * 9 / 5) + 32;
+        Console.WriteLine($"В Фаренгейтах: {fahrenheit:F2}°F");
+
+        Console.Write("Введите температуру в градусах Фаренгейта: ");
+        double f = double.Parse(Console.ReadLine());
+        double c = (f - 32) * 5 / 9;
+        Console.WriteLine($"В Цельсиях: {c:F2}°C");
+    }
+    
+}
---
+++ .net/homework-4/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Введите число: ");
+        string input = Console.ReadLine();
+
+        string[,] numbers = new string[10, 5]
+        {
+            { " 000 ", "0   0", "0   0", "0   0", " 000 " }, // 0
+            { "  1  ", " 11  ", "  1  ", "  1  ", " 111 " }, // 1
+            { " 222 ", "    2", " 222 ", "2    ", " 222 " }, // 2
+            { " 333 ", "    3", " 333 ", "    3", " 333 " }, // 3
+            { "4   4", "4   4", " 444 ", "    4", "    4" }, // 4
+            { " 555 ", "5    ", " 555 ", "    5", " 555 " }, // 5
+            { " 666 ", "6    ", " 666 ", "6   6", " 666 " }, // 6
+            { " 777 ", "    7", "   7 ", "  7  ", " 7   " }, // 7
+            { " 888 ", "8   8", " 888 ", "8   8", " 888 " }, // 8
+            { " 999 ", "9   9", " 999 ", "    9", " 999 " }  // 9
+        };
+
+        for (int row = 0; row < 5; row++)
+        {
+            foreach (char digit in input)
+            {
+                if (char.IsDigit(digit))
+                    Console.Write(numbers[digit - '0', row] + "  ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
