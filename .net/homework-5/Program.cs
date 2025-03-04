e37400ce88355723870e2b5d4472ba263f6d58bf
Mark Semenov <semenovmark07@gmail.com>
2025-03-04 05:01:57 +0200

    homework-4, homework-5

--- .net/homework-5/Program.cs
+++ .net/homework-5/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Address address = new Address("Одесса", "Садовая", 3);
+        int[] homework = { 10, 10, 10 };
+        int[] finals = { 10, 10 };
+        int[] exams = { 10, 10, 10 };
+
+        Student student = new Student(
+            "Иванов", "Иван", "Иванович", new DateTime(2000, 1, 11),
+            address, "+380 99 607 85 19", homework, finals, exams
+        );
+
+        student.PrintInfo();
+    }
+}
